6d09d9ccfb905d618dd8e75ffb6cd029495091c0
oriol <o.raventos.89@gmail.com>
2019-12-17 18:42:24 +0100

    ara mostra el missatge correcte, contant cada 10 segons: els nous numeros, els numeros duplicats i el total de numeros nous

--- Repository.py
+++ Repository.py
@@ -4,8 +4,9 @@ import os
 class Repository(object):
     def __init__(self,):
         self.numbers_list = []
-        # dir_path = os.path.dirname(os.path.realpath(__file__))
-        numbers_log = os.path.join(os.getenv('HOME'), 'numbers.log')
+        self.duplicated_numbers = 0
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        numbers_log = os.path.join(dir_path, 'numbers.log')
         logging.basicConfig(level=logging.DEBUG,
                             format='%(message)s',
                             filename=numbers_log,
@@ -15,6 +16,8 @@ class Repository(object):
         if number not in self.numbers_list:
             self.numbers_list.append(number)
             self.write_new_number(number)
+        else:
+            self.duplicated_numbers = self.duplicated_numbers + 1
 
     @staticmethod
     def write_new_number(number):
--- iskra_server.py
+++ iskra_server.py
@@ -8,10 +8,21 @@ import time
 from Controller import Controller
 from Repository import Repository
 
-def every_10_seg(numbers_list):
+def every_10_seg(repository):
+    signal_total = 0
+
     while run_server:
+        last_duplicate_count = repository.duplicated_numbers
         time.sleep(10)
-        print(numbers_list)
+
+        unique_numbers = len(repository.numbers_list) - signal_total
+        count_new_duplicates = repository.duplicated_numbers - last_duplicate_count
+        signal_total = len(repository.numbers_list)
+
+        #print(repository.numbers_list)
+        print(str(unique_numbers) + ' unique numbers, ' + str(count_new_duplicates) + ' duplicated received. Signal total: ' + str(signal_total))
+
+
 
 global run_server
 run_server = True
@@ -21,6 +32,9 @@ BUFFER_SIZE = 20
 global repository
 repository = Repository()
 controller = Controller(hostname, port, BUFFER_SIZE, repository, run_server)
-print_thread = threading.Thread(target=every_10_seg, args=(repository.numbers_list, ),  daemon=True)
-print_thread.start()
-controller.listen_to_requests()
+try:
+    print_thread = threading.Thread(target=every_10_seg, args=(repository, ),  daemon=True)
+    print_thread.start()
+    controller.listen_to_requests()
+except KeyboardInterrupt:
+    controller.server_socket.close()
